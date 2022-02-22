1f8077f6c4ab56d434974783943ccc3999f349c8
Mari0111 <nyalentine@gmail.com>
2022-02-22 11:20:01 +0800

    added icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,39 +15,91 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Flutter layout demo'),
           ),
-          body: Column(
+          body: Column(children: [
+            TitleRow(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ButtonColumn(label: 'CALL', icon: Icons.call),
+                ButtonColumn(label: 'ROUTE', icon: Icons.near_me),
+                ButtonColumn(label: 'SHARE', icon: Icons.share),
+              ],
+            )
+          ])),
+    );
+  }
+}
+
+class ButtonColumn extends StatelessWidget {
+  const ButtonColumn({
+    Key? key,
+    required this.label,
+    required this.icon,
+  }) : super(key: key);
+  final String label;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: Colors.blue),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TitleRow extends StatelessWidget {
+  const TitleRow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
             children: [
               Container(
-                padding: const EdgeInsets.all(32),
-                child: Row(children: [
-                  Expanded(
-                    child: Column(children: [
-                      Container(
-                        padding: const EdgeInsets.only(bottom: 8),
-                        child: const Text(
-                          'Oeschinen Lake Campground',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        'Kandersteg, Switzerland',
-                        style: TextStyle(
-                          color: Colors.grey[500],
-                        ),
-                      ),
-                    ]),
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
                   ),
-                  Icon(
-                    Icons.star,
-                    color: Colors.red[500],
-                  ),
-                  const Text('41'),
-                ]),
-              )
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
             ],
           )),
+          Icon(
+            Icons.star,
+            color: Colors.pink[500],
+          ),
+          const Text('41'),
+        ],
+      ),
     );
   }
 }
