0a2a9f286ca5f080d9c46c745733688c7c1dc2e9
Mari0111 <nyalentine@gmail.com>
2022-03-21 10:31:53 +0800

    navigate to secend screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,95 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My List"),
-      ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              leading: const Icon(
-                Icons.assignment_rounded,
-                color: Color.fromARGB(255, 33, 129, 36),
-              ),
-              shape: BeveledRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              title: Text('Төлбөр'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.savings_rounded,
-                  color: Color.fromARGB(255, 33, 129, 36)),
-              title: Text('Зорилготой хадгаламж'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.query_builder,
-                  color: Color.fromARGB(255, 33, 129, 36)),
-              title: Text('Миний санхүү'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.credit_card_rounded,
-                  color: Color.fromARGB(255, 33, 129, 36)),
-              title: Text('Карт'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.savings_rounded,
-                  color: Color.fromARGB(255, 33, 129, 36)),
-              title: Text('Дансны үйлчилгээ'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.phone_android_rounded,
-                  color: Color.fromARGB(255, 33, 129, 36)),
-              title: Text('Цахим банк'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                print("Moon");
-              },
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.compare_arrows,
-                  color: Color.fromARGB(255, 33, 129, 36)),
-              title: Text('Давтамжит гүйлгээ'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-          ),
-        ],
-      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/screens/second_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.red.shade100,
+        child: Center(
+          child: ElevatedButton(
+            child: Text('Go to second screen'),
+            onPressed: () {
+              print('Hello');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondScreen(),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
